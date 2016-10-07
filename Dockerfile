31f5d06256ff54e2724f0747311a3a7035023c6e
Steven Jack <stevenmajack@gmail.com>
2016-10-07 20:53:52 +0100

    Allow steps to be controlled via args

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ MAINTAINER charlie@vidsy.co
 ENV GLIDE_VERSION 0.10.2
 ENV GLIDE_DOWNLOAD_URL https://github.com/Masterminds/glide/releases/download/$GLIDE_VERSION/glide-$GLIDE_VERSION-linux-amd64.zip
 
-ADD ./build.sh /scripts/build.sh
-
 RUN apk update \
   && apk add openssh-client make git ca-certificates wget \
   && update-ca-certificates
@@ -14,5 +12,7 @@ RUN wget -O glide.zip "$GLIDE_DOWNLOAD_URL"
 RUN unzip glide.zip linux-amd64/glide
 RUN mv linux-amd64/glide /usr/local/bin
 RUN rm -rf linux-amd64 glide.zip
+
+ADD ./build.sh /scripts/build.sh
 
 ENTRYPOINT ["/scripts/build.sh"]
--- build.sh
+++ build.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-glide install
-CGO_ENABLED=0 go build -a -installsuffix nocgo .
+INSTALL="${1:-true}"
+BUILD="${2:-true}"
+SETUP_SSH="${3:-false}"
+USER=$(whoami)
+SSH_KEY_NAME="id_circleci_github"
+
+log () {
+	echo "[go-builder] => $1" 
+} 
+
+if [ "$SETUP_SSH" == "true" ]; then
+	log "Setting up SSH config for private repos"
+	mkdir -p /$USER/.ssh
+	echo -e "Host github.com\nIdentitiesOnly yes\nIdentityFile /$USER/.ssh/$SSH_KEY_NAME\nStrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" > /$USER/.ssh/config
+	chown -R $USER:$USER /$USER/.ssh
+	chmod 700 /$USER/.ssh
+	chmod 600 /$USER/.ssh/*
+fi
+
+if [ "$INSTALL" == "true" ]; then
+	log "Installing dependencies with glide"
+  glide install
+fi
+
+if [ "$BUILD" == "true" ]; then
+	log "Building binary"
+  CGO_ENABLED=0 go build -a -installsuffix nocgo .
+fi
