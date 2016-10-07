7322879e777dbd4ed5556e0619ad5e20795da9eb
Steven Jack <stevenmajack@gmail.com>
2016-10-07 20:56:06 +0100

    Fix spacing

--- build.sh
+++ build.sh
@@ -11,20 +11,20 @@ log () {
 } 
 
 if [ "$SETUP_SSH" == "true" ]; then
-	log "Setting up SSH config for private repos"
-	mkdir -p /$USER/.ssh
-	echo -e "Host github.com\nIdentitiesOnly yes\nIdentityFile /$USER/.ssh/$SSH_KEY_NAME\nStrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" > /$USER/.ssh/config
-	chown -R $USER:$USER /$USER/.ssh
-	chmod 700 /$USER/.ssh
-	chmod 600 /$USER/.ssh/*
+  log "Setting up SSH config for private repos"
+  mkdir -p /$USER/.ssh
+  echo -e "Host github.com\nIdentitiesOnly yes\nIdentityFile /$USER/.ssh/$SSH_KEY_NAME\nStrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" > /$USER/.ssh/config
+  chown -R $USER:$USER /$USER/.ssh
+  chmod 700 /$USER/.ssh
+  chmod 600 /$USER/.ssh/*
 fi
 
 if [ "$INSTALL" == "true" ]; then
-	log "Installing dependencies with glide"
+  log "Installing dependencies with glide"
   glide install
 fi
 
 if [ "$BUILD" == "true" ]; then
-	log "Building binary"
+  log "Building binary"
   CGO_ENABLED=0 go build -a -installsuffix nocgo .
 fi
