150c965909799d338e1bb6f9ce6ed1be40813297
Chris Reeves <chris.reeves@vidsy.co>
2020-04-29 09:33:18 +0100

    refactor: remove new ldflags

--- build.sh
+++ build.sh
@@ -2,16 +2,22 @@
 
 set -e
 
+USER=$(whoami)
 INSTALL="${INSTALL:-true}"
 BUILD="${BUILD:-true}"
-USER=$(whoami)
+BUILD_PATH="${BUILD_PATH:-.}"
+
 LDFLAGS=""
+
 VERSION_PACKAGE="${VERSION_PACKAGE:-main}"
 VERSION_VARIABLE_NAME="${VERSION_VARIABLE_NAME:-Version}"
-BUILD_PATH="${BUILD_PATH:-.}"
+VERSION_LDFLAG="-X ${VERSION_PACKAGE}.${VERSION_VARIABLE_NAME}"
+
 BUILD_TIME="${BUILD_TIME:-$(date -u +"%d/%m/%YT%H:%M:%S%z")}"
 BUILD_TIME_PACKAGE="${BUILD_TIME_PACKAGE:-main}"
 BUILD_TIME_VARIABLE_NAME="${BUILD_TIME_VARIABLE_NAME:-BuildTime}"
+BUILD_TIME_LDFLAG="-X ${BUILD_TIME_PACKAGE}.${BUILD_TIME_VARIABLE_NAME}"
+
 OUTPUT_ZONEINFO="${OUTPUT_ZONEINFO:-false}"
 
 _log () {
@@ -21,14 +27,8 @@ _log () {
 if [ -f VERSION ]; then
 	_log "Version file found, adds ld flags"
 
-	# Sets version / build time ld flags for:
-	# - legacy services where variables are in the services main
-	# - services that use the new x/service package - this will need to be changed when x/service
-	#   is promoted to the main pkg - during that transition we should support both
-	for FLAG in "-X ${VERSION_PACKAGE}.${VERSION_VARIABLE_NAME}=$(cat VERSION)" \
-				"-X ${BUILD_TIME_PACKAGE}.${BUILD_TIME_VARIABLE_NAME}=${BUILD_TIME}" \
-				"-X github.com/vidsy/back-end/pkg/x/service.version=$(cat VERSION)" \
-				"-X github.com/vidsy/back-end/pkg/x/service.built=${BUILD_TIME}"
+	for FLAG in "${VERSION_LDFLAG}=$(cat VERSION)" \
+				"${BUILD_TIME_LDFLAG}=${BUILD_TIME}"
 		do
 		LDFLAGS="${LDFLAGS}${FLAG} "
  	done
