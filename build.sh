6d60e0d174300124fd1f8da3e34251f1b5524e47
Chris Reeves <hello@chris.reeves.io>
2020-05-06 10:31:03 +0100

    Add support for x/service ld flags (#30)
    * feat: add support for x/service ld flags
    
    * chore: bump
    
    * fix: quote flags
    
    * feat: moving sed
    
    * refactor: use list
    
    * fix: sh
    
    * refactor: remove new ldflags

--- build.sh
+++ build.sh
@@ -2,16 +2,22 @@
 
 set -e
 
+USER=$(whoami)
 INSTALL="${INSTALL:-true}"
 BUILD="${BUILD:-true}"
-USER=$(whoami)
-VERSION_FLAGS=""
+BUILD_PATH="${BUILD_PATH:-.}"
+
+LDFLAGS=""
+
 VERSION_PACKAGE="${VERSION_PACKAGE:-main}"
 VERSION_VARIABLE_NAME="${VERSION_VARIABLE_NAME:-Version}"
-BUILD_PATH="${BUILD_PATH:-.}"
+VERSION_LDFLAG="-X ${VERSION_PACKAGE}.${VERSION_VARIABLE_NAME}"
+
 BUILD_TIME="${BUILD_TIME:-$(date -u +"%d/%m/%YT%H:%M:%S%z")}"
 BUILD_TIME_PACKAGE="${BUILD_TIME_PACKAGE:-main}"
 BUILD_TIME_VARIABLE_NAME="${BUILD_TIME_VARIABLE_NAME:-BuildTime}"
+BUILD_TIME_LDFLAG="-X ${BUILD_TIME_PACKAGE}.${BUILD_TIME_VARIABLE_NAME}"
+
 OUTPUT_ZONEINFO="${OUTPUT_ZONEINFO:-false}"
 
 _log () {
@@ -20,7 +26,12 @@ _log () {
 
 if [ -f VERSION ]; then
 	_log "Version file found, adds ld flags"
-	VERSION_FLAGS="-X ${VERSION_PACKAGE}.${VERSION_VARIABLE_NAME}=$(cat VERSION) -X ${BUILD_TIME_PACKAGE}.${BUILD_TIME_VARIABLE_NAME}=${BUILD_TIME}"
+
+	for FLAG in "${VERSION_LDFLAG}=$(cat VERSION)" \
+				"${BUILD_TIME_LDFLAG}=${BUILD_TIME}"
+		do
+		LDFLAGS="${LDFLAGS}${FLAG} "
+ 	done
 fi
 
 if [ "${INSTALL}" == "true" ]; then
@@ -33,9 +44,11 @@ if [ "${INSTALL}" == "true" ]; then
   fi
 fi
 
+LDFLAGS=${LDFLAGS} | sed 's/ *$//g'
+
 if [ "${BUILD}" == "true" ]; then
   _log "Building binary"
-  go build -i -ldflags "$VERSION_FLAGS" -a -installsuffix nocgo ${BUILD_PATH}
+  go build -i -ldflags "$LDFLAGS" -a -installsuffix nocgo ${BUILD_PATH}
 fi
 
 if [ "${OUTPUT_ZONEINFO}" == "true" ]; then
